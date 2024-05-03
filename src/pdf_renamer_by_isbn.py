8b07a866e9f30426a331c9594e62a929e14afb63
esuji5 <esuji5@gmail.com>
2024-05-04 01:58:19 +0900

    処理を最新に：src/pdf_renamer_by_isbn.py

--- src/pdf_renamer_by_isbn.py
+++ src/pdf_renamer_by_isbn.py
@@ -1,113 +1,95 @@
-# require: zbar, ImageMagick
+# require: zbar
 # TODO: 引数で処理済みのファイルも処理対象にしたりFIND_PAGEを調整したりできるといいなあ
-import glob
 import os
 import re
-import subprocess
 import sys
 import unicodedata
 from time import sleep
+from typing import Optional
 from urllib.error import HTTPError
 
-import utils
-from requests_html import HTMLSession
+import fitz
+from PIL import Image
+from pyzbar.pyzbar import decode
+from requests_html import HTMLResponse, HTMLSession
 
+import utils
+
+KINOKUNIYA_ZASSHI_SEARCH_URL = "https://www.kinokuniya.co.jp/f/dsg-04-"
 KINOKUNIYA_SEARCH_URL = (
     "https://www.kinokuniya.co.jp/disp/CSfDispListPage_001.jsp?qsd=true&ptk=01&gtin="
 )
-KINOKUNIYA_ZASSHI_SEARCH_URL = "https://www.kinokuniya.co.jp/f/dsg-04-"
 
-RE_PAGE = re.compile("Pages:\s*[0-9]{1,}")
 RE_KANJI = re.compile("(<|〈).+?(>|〉)")
 RE_ISBN = re.compile("(978[0-9]{10}|491[0-9]{10})")
 NO_RESULT_MESSAGE = "該当する結果がありません"
-FIND_PAGE = 3  # 後ろからどれだけのページを探索するか
+FIND_PAGE = 3  # 最初と最後からどれだけのページを探索するか
 
 
-def pdf_to_isbn(pdf_path, from_the_back=True):
-    # print('pdf_path:', pdf_path)
-    def remove_tmp_img(images_path):
-        # 一時ファイルを削除
-        tmp_img_list = glob.glob(images_path)
-        for tmp_img in tmp_img_list:
-            os.remove(tmp_img)
+def fetch_isbn_from_pdf(pdf_path: str, from_the_back: bool = True) -> str:
+    """PDFファイルからISBNを抽出する。
 
-    # ページ数を取得
-    args_pdfinfo = ["pdfinfo", pdf_path]
-    pdfinfo_result = subprocess.check_output(args_pdfinfo)
-    page = RE_PAGE.search(str(pdfinfo_result)).group(0).split(" ")[-1]
+    Args:
+        pdf_path (str): PDFファイルのパス。
+        from_the_back (bool): ページの後ろから探索するかどうか。
 
-    # 一時ファイル名を設定
-    tmp_file = "tmp-img"
-    img_path = os.path.join(os.path.dirname(pdf_path), tmp_file)
-    images_path = f"{img_path}*"
+    Returns:
+        Optional[str]: 抽出されたISBNコード。見つからない場合はNone。
+    """
+    with utils.timer("fetch_isbn_from_pdf: " + pdf_path):
+        doc = fitz.open(pdf_path)  # ドキュメントを開く
+        for page_idx, page in enumerate(
+            reversed(doc)
+        ):  # ドキュメントのページを反復処理する
+            if FIND_PAGE < page_idx <= (len(doc) - FIND_PAGE):
+                continue
+            xref = page.get_images()[0][0]  # 画像のXREF番号を取得する
+            pix = fitz.Pixmap(doc, xref)
+            if pix.n < 5:  # 画像がRGBAでない場合は変換する
+                pix = fitz.Pixmap(fitz.csRGB, pix)
+            image = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
+            decoded_list = decode(image)
+            jan_code_list = [
+                dd.data.decode()
+                for dd in decoded_list
+                if dd.data.decode().startswith(("978", "491"))
+            ]
 
-    # 存在する一時ファイルがあれば削除しておく
-    remove_tmp_img(images_path)
-
-    # target_page = 0
-    # if from_the_back:
-    #     target_page =
-    # 対象のページを一時ファイルとしてjpgに切り出す
-    args_pdfimages_from_tail_page = [
-        "pdfimages",
-        "-j",
-        "-f",
-        str(int(page) - FIND_PAGE + 1),
-        pdf_path,
-        img_path + "_t",
-    ]
-    subprocess.check_call(args_pdfimages_from_tail_page)
-    args_pdfimages_from_head_page = [
-        "pdfimages",
-        "-j",
-        "-f",
-        "1",
-        "-l",
-        str(FIND_PAGE),
-        pdf_path,
-        img_path + "_h",
-    ]
-    subprocess.check_call(args_pdfimages_from_head_page)
-    # jpgからバーコードを読み取り、ISBNがあれば返す
-    tmp_img_list = glob.glob(images_path)
-    isbn = None
-    for tmp_img in tmp_img_list:
-        if not tmp_img.endswith("jpg"):
-            continue
-        args3 = ["zbarimg", "-q", tmp_img]
-        p3 = subprocess.Popen(
-            args3,
-            stdin=subprocess.PIPE,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            shell=False,
-        )
-        out = str(p3.stdout.readlines())
-        if RE_ISBN.search(out):
-            isbn = RE_ISBN.search(out).group(0)
-            print("found isbn:", isbn)
-            break
-
-    # 一時ファイルを削除
-    remove_tmp_img(images_path)
-    return isbn
+            if jan_code_list:
+                return jan_code_list[0]
+    return ""
 
 
-# 紀伊國屋書店の検索をスクレイピング
-def item_search(isbn):
+def item_search(isbn: str) -> Optional[HTMLResponse]:
+    """紀伊國屋書店のウェブサイトでISBNに基づいて商品を検索する。
+
+    Args:
+        isbn (str): 検索する商品のISBN。
+
+    Returns:
+        Optional[HTMLResponse]: 検索結果のHTMLレスポンス。該当する商品がない場合はNone。
+    """
     """:return: requests_html.HTMLResponse"""
     if isbn.startswith("491"):
         res = session.get(KINOKUNIYA_ZASSHI_SEARCH_URL + isbn)
     else:
         res = session.get(KINOKUNIYA_SEARCH_URL + isbn)
     if NO_RESULT_MESSAGE in res.text:
-        return
+        return None
     else:
         return res
 
 
-def get_book_data(res):
+def get_book_data(isbn: str, res: HTMLResponse) -> tuple[str, str, str, str]:
+    """HTMLレスポンスから書籍または雑誌のデータを抽出する。
+
+    Args:
+        isbn (str): 検索した書籍または雑誌のISBN。
+        res (HTMLResponse): 紀伊國屋書店の検索結果ページのHTMLレスポンス。
+
+    Returns:
+        Tuple[str, str, str, str]: 書籍または雑誌のタイトル、著者（雑誌の場合は空文字）、出版社、発行日を含むタプル。
+    """
     if isbn.startswith("491"):  # 雑誌
         author = ""
         page_title = res.html.find("title")[0].text
@@ -123,25 +105,37 @@ def get_book_data(res):
         author = res.html.find(
             "#main_contents > form > div.list_area_wrap > div > div.listrightbloc > div.details > p"
         )[0].text
-        publisher = res.html.find(
+        publisher_info = res.html.find(
             "#main_contents > form > div.list_area_wrap > div > div.listrightbloc > div.details2.select_section1 > ul > li:nth-child(1)"
         )[0].text
     title = page_title.split(" - ")[0]
-    title, page_title, author, publisher = [
-        unicodedata.normalize("NFKC", t) for t in [title, page_title, author, publisher]
+    title, page_title, author, publisher_info = [
+        unicodedata.normalize("NFKC", t)
+        for t in [title, page_title, author, publisher_info]
     ]
     search_kanji = RE_KANJI.search(page_title)
     if search_kanji:
         kanji = search_kanji.group(0).replace("〈", "").replace("〉", "")
         title = f"{title} {kanji}巻"
-    # import ipdb;ipdb.set_trace()
-    publisher, publish_date = publisher.split("(")
-    return title, author, publisher, "(" + publish_date
+    publisher_split = publisher_info.split("(")
+
+    publisher = "(".join(publisher_split[:-1])
+    publish_date = "(" + publisher_split[-1]
+    return title, author, publisher, publish_date
 
 
-# 取得したxml(item)から必要な情報を抜き出して新しいファイル名を返す
-def get_new_name(r):
-    title, author, publisher, publish_date = get_book_data(r)
+def get_new_name(isbn: str, res: HTMLResponse) -> str:
+    """
+    ISBNとHTMLレスポンスから新しいファイル名を生成する。
+
+    Args:
+        isbn (str): 書籍または雑誌のISBN。
+        res (HTMLResponse): 紀伊國屋書店の検索結果ページのHTMLレスポンス。
+
+    Returns:
+        str: 新しいファイル名。
+    """
+    title, author, publisher, publish_date = get_book_data(isbn, res)
     name_dict = {
         "title": title,
         "author": author,
@@ -157,21 +151,30 @@ def get_new_name(r):
     return newname.replace("/", "／")  # ファイルシステム上の禁則文字をreplaceする
 
 
-def fetch_kinokuniya_item(isbn):
+def fetch_kinokuniya_item(isbn: str) -> Optional[HTMLResponse]:
+    """
+    ISBNを使用して紀伊國屋書店のアイテムを検索し、HTMLレスポンスを取得する。
+
+    Args:
+        isbn (str): 検索するアイテムのISBNコード。
+
+    Returns:
+        Optional[HTMLResponse]: 検索結果のHTMLレスポンス。該当するアイテムがない場合はNoneを返す。
+    """
     req_count = 0
-    # 5回までamazonのAPIにリクエストを投げてみる
-    while req_count < 5:
+    # 3回までリクエストを投げてみる
+    while req_count < 3:
         try:
             items = item_search(isbn)
             if items:
                 return items
             else:
-                return
+                return None
         except HTTPError as e:
             # エラーが出たら( ˘ω˘)ｽﾔｧ
             print(e)
             req_count += 1
-            if req_count < 5:
+            if req_count < 3:
                 # sleep_timeが徐々に伸びるように設定
                 sleep_time = 2**req_count
                 print("retry after {} second".format(sleep_time))
@@ -179,7 +182,7 @@ def fetch_kinokuniya_item(isbn):
             else:
                 # 呼び出し元でエラー内容等を表示
                 raise
-    return
+    return None
 
 
 if __name__ == "__main__":
@@ -189,21 +192,15 @@ if __name__ == "__main__":
 
     session = HTMLSession()
     # 処理済みのファイルを雑に判別して除外
-    # pdf_path_list = [
-    #     p for p in pdf_path_list if not os.path.basename(p).startswith("[")
-    # ]
     pdf_path_list = [
-        p
-        for p in pdf_path_list
-        if os.path.basename(p) == "[芳文社]まんがタイムきらら 2016年 02 月号 [雑誌]_4910083450165.pdf"
+        p for p in pdf_path_list if not os.path.basename(p).startswith("[")
     ]
     for pdf_path in pdf_path_list:
         print(pdf_path)
-        isbn = pdf_to_isbn(pdf_path)
+        isbn = fetch_isbn_from_pdf(pdf_path)
         if not isbn:
             print("Not found isbn")
             continue
-
         try:
             kinokuniya_item = fetch_kinokuniya_item(isbn)
         except HTTPError as e:
@@ -213,7 +210,7 @@ if __name__ == "__main__":
             print(e, pdf_path, isbn)
             continue
         if kinokuniya_item:
-            new_name = get_new_name(kinokuniya_item)
+            new_name = get_new_name(isbn, kinokuniya_item)
         else:
             new_name = str(isbn) + ".pdf"
             # pdfファイルをリネーム
